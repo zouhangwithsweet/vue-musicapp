e193fa600fd25ca78f4b80dcab8b6b54f2e0f4d4
zouhang <1102639563@qq.com>
2017-07-16 16:59:53 +0800

    0.1

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="recommend">
         <div class="recommend-content">
-            <div class="slider-wrapper">
-
+            <div v-if="recommends.length" class="slider-wrapper">
+                <slider>
+                    <div v-for="(item, index) in recommends" :key="item">
+                        <a :href="item.linkUrl">
+                            <img :src="item.picUrl"/>
+                        </a>
+                    </div>
+                </slider>
             </div>
             <div class="recommend-list">
                 <h1 class="list-title">热门歌单推荐</h1>
@@ -17,7 +23,13 @@
 <script>
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
+import Slider from 'base/slider/slider'
 export default {
+    data() {
+        return {
+            recommends: []
+        }
+    },
     created() {
         this._getRecommend()
     },
@@ -25,10 +37,13 @@ export default {
         _getRecommend() {
             getRecommend().then((res) => {
                 if(res.code === ERR_OK){
-                    console.log(res.data.slider)
+                    this.recommends = res.data.slider;
                 }
             })
         }
+    },
+    components: {
+        Slider
     }
 }
 </script>
