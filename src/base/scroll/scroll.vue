94dbf8b5db7b9a06fda4a745439fdb09b4f5dcd1
zouhang <1102639563@qq.com>
2017-07-17 14:38:48 +0800

    歌单渲染，滚动组件

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,59 @@
+<template>
+    <div ref="wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+    props: {
+        probeType: {
+            type: Number,
+            default: 1
+        },
+        click: {
+            type: Boolean,
+            default: true
+        },
+        data: {
+            type: Array,
+            default: null
+        }
+    },
+    watch: {
+        data() {
+            setTimeout(() => {
+                this.refresh()
+            },20)
+        }
+    },
+    mounted() {
+        this._initBscroll()
+    },
+    methods: {
+        _initBscroll() {
+            if(!this.$refs.wrapper) {
+                return
+            }
+            this.scroll = new BScroll(this.$refs.wrapper, {
+                probeType: this.probeType,
+                click: this.click
+            })
+        },
+        enable() {
+            this.scroll && this.scroll.enable()
+        },
+        disable() {
+            this.scroll && this.scroll.disable()
+        },
+        refresh() {
+            this.scroll && this.scroll.refresh()
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,33 +1,45 @@
 <template>
     <div class="recommend">
-        <div class="recommend-content">
-            <div v-if="recommends.length" class="slider-wrapper">
-                <slider>
-                    <div v-for="(item, index) in recommends" :key="item">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl"/>
-                        </a>
-                    </div>
-                </slider>
+        <scroll class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper">
+                    <slider>
+                        <div v-for="(item, index) in recommends" :key="item">
+                            <a :href="item.linkUrl">
+                                <img :src="item.picUrl"/>
+                            </a>
+                        </div>
+                    </slider>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li v-for="(item, index) in discList" :key="item" class="item">
+                            <div class="icon">
+                                <img :src="item.imgurl" width="60" height="60">
+                            </div>
+                            <div class="text">
+                                <h2 class="name" v-html="item.creator.name"></h2>
+                                <p class="desc" v-html="item.dissname"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul>
-
-                </ul>
-            </div>
-        </div>
+        </scroll>
     </div>
 </template>
 
 <script>
+import Scroll from 'base/scroll/scroll'
 import {getRecommend, getDistList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 import Slider from 'base/slider/slider'
 export default {
     data() {
         return {
-            recommends: []
+            recommends: [],
+            discList: []
         }
     },
     created() {
@@ -44,14 +56,15 @@ export default {
         },
         _getDistList() {
             getDistList().then((res) => {
-                if(res.data === ERR_OK){
-                    console.log(res.data)
+                if(res.code === ERR_OK){
+                    this.discList = res.data.list
                 }
             })
         }
     },
     components: {
-        Slider
+        Slider,
+        Scroll
     }
 }
 </script>
