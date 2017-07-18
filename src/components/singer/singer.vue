b9935a687b9a817043d711a1838f72347edaa686
zouhang <1102639563@qq.com>
2017-07-18 20:09:47 +0800

    singer组件

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,64 @@
 <template>
-    <div>排行</div>
+    <div class="singer">
+        
+    </div>
 </template>
 
 <script>
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 export default {
-  
+    data() {
+        return {
+            singers: []
+        }
+    },
+    created() {
+        this._getSingerList()
+    },
+    methods: {
+        _getSingerList() {
+            getSingerList().then((res) => {
+                if (res.code === ERR_OK) {
+                    this.singers  = res.data.list
+                }
+            })
+        },
+        _normlizeSinger(list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
+                }
+            }
+
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push({
+                        id: item.Fsinger_mid,
+                        name: item.Fsinger_name,
+                        avatar: `https://y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000` 
+                    })
+                }
+                const key = item.Findex
+                if(!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items: []
+                    }
+                }
+            });
+        }
+    }
 }
 </script>
 
-<style lang="stylus">
-
+<style scoped lang="stylus">
+.singer
+    position fixed
+    top 88px
+    bottom 0
+    width 100%
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Recommend from 'components/recommend/recommend'
 import Singer from 'components/singer/singer'
 import Rank from 'components/rank/rank'
 import Search from 'components/search/search'
+import SingerDetail from 'components/singer-detail/singer-detail'
 Vue.use(Router)
 
 export default new Router({
@@ -20,7 +21,13 @@ export default new Router({
         {
             path: '/singer',
             name: 'singer',
-            component: Singer
+            component: Singer,
+            children: [
+                {
+                    path: ':id',
+                    component: SingerDetail
+                }
+            ]
         },
         {
             path: '/rank',
