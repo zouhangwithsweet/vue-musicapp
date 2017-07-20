bc80f6f2a3d18c69ca114ab46a21778a41cf28ae
zouhang <1102639563@qq.com>
2017-07-20 09:39:28 +0800

    歌手列表组件

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="singer">
-        
+        <list-view :data="singers"></list-view>
     </div>
 </template>
 
 <script>
 import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
 export default {
@@ -22,7 +24,7 @@ export default {
         _getSingerList() {
             getSingerList().then((res) => {
                 if (res.code === ERR_OK) {
-                    this.singers  = res.data.list
+                    this.singers = this._normlizeSinger(res.data.list)
                 }
             })
         },
@@ -36,11 +38,10 @@ export default {
 
             list.forEach((item, index) => {
                 if (index < HOT_SINGER_LEN) {
-                    map.hot.items.push({
+                    map.hot.items.push(new Singer({
                         id: item.Fsinger_mid,
-                        name: item.Fsinger_name,
-                        avatar: `https://y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000` 
-                    })
+                        name: item.Fsinger_name
+                    }))
                 }
                 const key = item.Findex
                 if(!map[key]) {
@@ -49,8 +50,31 @@ export default {
                         items: []
                     }
                 }
-            });
+                map[key].items.push(new Singer({
+                    id: item.Fsinger_mid,
+                    name: item.Fsinger_name
+                }))
+            })
+            // 洗map数据
+            let hot = []
+            let ret = []
+            for (let key in map) {
+                let val = map[key]
+                if (val.title.match(/[a-zA-Z]/)) {
+                    ret.push(val)
+                } else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
+            }
+            ret.sort((a,b) => {
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+
+            return hot.concat(ret)
         }
+    },
+    components: {
+        ListView
     }
 }
 </script>
