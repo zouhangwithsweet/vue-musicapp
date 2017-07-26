a4735c2507505ae38a97a0ce9f7872462401cf19
zouhang <1102639563@qq.com>
2017-07-26 08:41:51 +0800

    快速选择组件

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,7 +1,7 @@
 <template>
-    <scroll class="listview" :data="data">
+    <scroll class="listview" :data="data" ref="listView">
         <ul>
-            <li v-for="group in data" class="list-group" :key="group">
+            <li v-for="group in data" class="list-group" :key="group" ref="listGroup">
                 <h2 class="list-group-title">{{group.title}}</h2>
                 <ul>
                     <li v-for="item in group.items" class="list-group-item" :key="item">
@@ -11,17 +11,29 @@
                 </ul>
             </li>
         </ul>
+        <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+            <ul>
+                <li v-for="(item, index) in shortcutList" class="item" :key="item" :data-index="index">
+                    {{item}}
+                </li>
+            </ul>
+        </div>
     </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import {getData} from 'common/js/dom'
+const ANCHOR_HEIGHT = 18
 export default {
     props: {
         data: {
             type: Array,
             default: []
         }
+    },
+    created() {
+        this.touch = {}
     },
     computed: {
         shortcutList() {
@@ -32,6 +44,25 @@ export default {
     },
     components: {
         Scroll
+    },
+    methods: {
+        onShortcutTouchStart(e) {
+            let anchorIndex = getData(e.target, 'index')
+            let firstTouch = e.touches[0]
+            this.touch.y1 = firstTouch.pageY
+            this.touch.anchorIndex = anchorIndex
+            this._scrollTo(anchorIndex)
+        },
+        onShortcutTouchMove(e) {
+            let firstTouch = e.touches[0]
+            this.touch.y2 = firstTouch.pageY
+            let delta = (this.touch.y2 -this.touch.y1) / ANCHOR_HEIGHT | 0
+            let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+            this._scrollTo(anchorIndex)
+        },
+        _scrollTo(index) {
+            this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 200)
+        }
     }
 }
 </script>
