29e7fca3baf9c2a200e53f2007571161bbfb8578
zouhang <1102639563@qq.com>
2017-07-29 19:48:41 +0800

    功能完善

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -27,13 +27,21 @@
                 </li>
             </ul>
         </div>
+        <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+            <h1 class="fixed-title">{{fixedTitle}}</h1>
+        </div>
+        <div v-show="!data.length" class="loading-container">
+            <Loading></Loading>
+        </div>
     </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import {getData} from 'common/js/dom'
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 export default {
     props: {
         data: {
@@ -44,7 +52,8 @@ export default {
     data() {
         return {
             scrollY: -1,
-            currentIndex: 0
+            currentIndex: 0,
+            diff: -1
         }
     },
     created() {
@@ -58,6 +67,13 @@ export default {
             return this.data.map((group) => {
                 return group.title.substr(0, 1)
             })
+        },
+        fixedTitle() {
+            if (this.scrollY > 0) {
+                return ''
+            }
+            return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+
         }
     },
     watch: {
@@ -69,7 +85,7 @@ export default {
         scrollY(newVal) {
             const listHeight = this.listHeight
             if (newVal > 0) {
-                this.currenIndex = 0
+                this.currentIndex = 0
                 return
             }
             for (let i = 0; i < listHeight.length; i++) {
@@ -77,14 +93,24 @@ export default {
                 let height2 = listHeight[i + 1]
                 if(!height2 || -newVal<height2) {
                     this.currentIndex = i
+                    this.diff = height2 + newVal
                     return
                 }
             }
             this.currentIndex = listHeight.length - 2
+        },
+        diff(newVal) {
+            let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+            if (this.fixedTop === fixedTop) {
+                return
+            }
+            this.fixedTop = fixedTop
+            this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
         }
     },
     components: {
-        Scroll
+        Scroll,
+        Loading
     },
     methods: {
         onShortcutTouchStart(e) {
