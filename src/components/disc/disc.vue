862bee583dc5ac308b919d3328aa897e5b0be16c
zouhang <80583600@qq.com>
2017-09-04 19:52:47 +0800

    recommend, rank update

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,13 +1,21 @@
 <template>
     <transition name="slide">
-        <music-list :title="title" :bg-image="bgImage"></music-list>
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
     </transition>
 </template>
 
 <script>
 import musicList from 'components/music-list/music-list'
 import {mapGetters} from 'vuex'
+import {getSongList} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 export default {
+    data() {
+        return {
+            songs: []
+        }
+    },
     computed: {
         title() {
             return this.disc.dissname
@@ -18,6 +26,30 @@ export default {
         ...mapGetters([
             'disc'
         ])  
+    },
+    created () {
+        this._getSongList()
+    },
+    methods: {
+        _getSongList() {
+            if(!this.disc.dissid) {
+                this.$router.push('/recommend')
+            }
+            getSongList(this.disc.dissid).then((res) => {
+                if (res.code === ERR_OK) {
+                    this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+                }
+            })
+        },
+        _normalizeSongs(list) {
+            let ret = []
+            list.forEach((musicData) => {
+                if(musicData.songid && musicData.albummid) {
+                    ret.push(createSong(musicData))
+                }
+            })
+            return ret
+        }
     },
     components: {
         musicList
